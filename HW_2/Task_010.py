1f611eb6781f7abf569efe0ea66a77d8c3aa9ac3
Anton Kommer <lipokuza@rambler.ru>
2023-03-13 05:13:45 +0300

    add: Task_010.py

--- HW_2/Task_010.py
+++ HW_2/Task_010.py
@@ -1,1 +1,13 @@
-# HW_2 Task_010 is done.
+n = int(input('Input number of coins: '))
+count_eagle = 0
+count_tail = 0
+for i in range(n):
+    x = int(input('Input 0 for eagle coin and 1 for tail coin: '))
+    if x == 0:
+        count_eagle += 1
+    else:
+        count_tail += 1
+if count_tail > count_eagle:
+    print("It is needed to flip at least", count_eagle, "eagle coins to turn every coin to tail position")
+else:
+    print("It is needed to flip at least", count_tail, "tail coins to turn every coin to eagle position")
