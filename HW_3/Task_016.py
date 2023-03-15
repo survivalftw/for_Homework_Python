d25cd1757359b745c18327f254b7c452d529e453
Anton Kommer <lipokuza@rambler.ru>
2023-03-16 00:35:36 +0300

    add: Task_016.py

---
+++ HW_3/Task_016.py
@@ -0,0 +1,5 @@
+n = int(input('Input list lengh: '))
+Given_list = [int(input('Input list element: ')) for i in range (n)]
+print (Given_list)
+Track_number = int(input('Input a number to track in list: '))
+print (f'Number {Track_number} occurs in list {Given_list.count(Track_number)} times')
