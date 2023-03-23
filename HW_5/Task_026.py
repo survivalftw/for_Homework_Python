28649d289660913f440f62d4f7f00bc53ceb72e3
Anton Kommer <lipokuza@rambler.ru>
2023-03-23 12:34:31 +0300

    fix: Task_026.py

--- HW_5/Task_026.py
+++ HW_5/Task_026.py
@@ -1,8 +1,8 @@
 Number = int(input('Input a number: '))
 PowerDegree = int(input('Input a power degree: '))
 def power_degree_recursive(m, n):
-    if m == 1:
-        return m
+    if m == 0:
+        return m + 1
     else:
         return n * power_degree_recursive(m-1, n)
-print(power_degree_recursive(PowerDegree + 1, Number))
+print(power_degree_recursive(PowerDegree, Number))
