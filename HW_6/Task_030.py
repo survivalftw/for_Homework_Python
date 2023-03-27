3317cfd82295184f3547d07fd9aa6fd5365478a5
Anton Kommer <lipokuza@rambler.ru>
2023-03-27 03:30:50 +0300

    add: Task_030.py

---
+++ HW_6/Task_030.py
@@ -0,0 +1,5 @@
+a = int(input('Input start number: '))
+d = int(input('Input increment multiplier progression number: '))
+n = int(input('Input progression length number: '))
+for i in range(n):
+    print(a + i * d)
